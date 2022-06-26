0015ee55e74eec70165ca9d6747580fd3bc6e712
tanvirch0750 <tanvir0750@yahoo.com>
2022-06-26 19:56:57 +0600

    showing popup when clik marker

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -24,12 +24,20 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    // show popup when click marker
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'hi there',
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
