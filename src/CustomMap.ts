bdfd49599688904c103f5aeb919d0225ce063c61
tanvirch0750 <tanvir0750@yahoo.com>
2022-06-26 20:25:09 +0600

    wrapped maps app

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,6 +1,6 @@
 // instruction to every other class
 // on how they can be an argument to 'addMarker'
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
