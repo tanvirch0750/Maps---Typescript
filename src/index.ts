c759093a5aad5d8f8efb2b813c799a1ae6dc8347
tanvirch0750 <tanvir0750@yahoo.com>
2022-06-26 17:25:10 +0600

    implemented custom maps and show marker

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 const company = new Company();
-console.log(user);
-console.log(company);
+
+const customMap = new CustomMap('map');
+
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
